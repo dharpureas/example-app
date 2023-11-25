b1eb51ce12c491d6e785f78a4b213a03a35a1efa
dharpureas <aayushdharpure@gmail.com>
2023-11-25 16:12:12 +0530

    Update hello world program for readability and maintainability

--- practicals/hello.php
+++ practicals/hello.php
@@ -1,3 +1,19 @@
 <?php
     echo "Hello, World";
+?><?php
+
+/**
+ * Prints a friendly greeting.
+ *
+ * @param string $name The name to include in the greeting.
+ */
+function printGreeting($name) {
+    echo "Hello, $name! Welcome to the world of PHP.\n";
+}
+
+// Example usage
+$visitorName = "John";
+printGreeting($visitorName);
+
 ?>
+
