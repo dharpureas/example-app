ce49cc47ff033eae733ec7b05521ec921653cf80
dharpureas <aayushdharpure@gmail.com>
2023-11-25 16:16:33 +0530

    Update Number swapping program for readability and maintainability

--- practicals/swapping.php
+++ practicals/swapping.php
@@ -1,12 +1,18 @@
+
 <?php
-function swapNumbers($num1, $num2) {
+
+/**
+ * Swaps the values of two numbers and prints the result.
+ *
+ * @param int $num1 The first number.
+ * @param int $num2 The second number.
+ */
+function swapNumbers(&$num1, &$num2) {
     echo "Before swapping: ";
     echo "Number 1 = $num1, Number 2 = $num2\n";
 
     // Swapping logic
-    $temp = $num1;
-    $num1 = $num2;
-    $num2 = $temp;
+    [$num1, $num2] = [$num2, $num1];
 
     echo "After swapping: ";
     echo "Number 1 = $num1, Number 2 = $num2\n";
@@ -17,5 +23,7 @@ $number1 = 5;
 $number2 = 10;
 
 swapNumbers($number1, $number2);
+
 ?>
+
 
