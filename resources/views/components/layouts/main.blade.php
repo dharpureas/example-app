a3f8541ca63699eb141b1efdf4cee643bed94963
dharpureas <aayushdharpure@gmail.com>
2023-12-01 15:11:33 +0530

    Changes done

---
+++ resources/views/components/layouts/main.blade.php
@@ -0,0 +1,14 @@
+<div>
+    <!-- Always remember that you are absolutely unique. Just like everyone else. - Margaret Mead -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3>HELLO WORLD</h3>
+</body>
+</html>
+</div>
