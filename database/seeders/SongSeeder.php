2a6724fbe3fff6c2cf656ea93b9509ed921f4af9
dharpureas <aayushdharpure@gmail.com>
2023-12-10 00:33:51 +0530

    Render songs from Song objects

--- database/seeders/SongSeeder.php
+++ database/seeders/SongSeeder.php
@@ -1,12 +1,10 @@
 <?php
 
 namespace Database\Seeders;
-
+use App\Models\Song;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\Song;
- 
- 
+
 class SongSeeder extends Seeder
 {
     /**
@@ -14,21 +12,17 @@ class SongSeeder extends Seeder
      */
     public function run(): void
     {
+
         // Seed a few songs
         Song::create([
-            'sr' => '1',
             'artist' => 'Eminem',
             'title' => 'Stan',
-            'album' => 'The Marshall Mathers LP',
-            'duration' => '6:44',
         ]);
 
         Song::create([
-        'sr' => '2',
             'artist' => 'A P Dhillon',
             'title' => 'With You',
-            'album' => 'The Marshall Mathers LP',
-            'duration' => '6:44',
         ]);
+
     }
-}
+    }
